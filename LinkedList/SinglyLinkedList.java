5bf7edced343d75c0f13b839f21c899d4bea4fe3
Ashish Yadav <ydvashish1983@gmail.com>
2022-08-21 08:56:58 +0530

    Update SinglyLinkedList.java

--- LinkedList/SinglyLinkedList.java
+++ LinkedList/SinglyLinkedList.java
@@ -1,19 +1,19 @@
 package LinkedList;
 
-public class SinglyLinkedList {
+public class SinglyLinkedList {     // This class shows hoe Singly Linked List is Implemented in Java
     private static Node head;
     public static void main(String[] args){
-
+        // Call Methods to run here
     }
 
-
+    // This Method add a Node to Front of the Single Linked List
     private static void addToFront(int data){
         Node newNode = new Node(data);
         newNode.next = head;
         head = newNode;
     }
 
-
+    // This Method adds a Node to the Back of the Singly Linked List
     private static void addToBack(int data){
         if(head == null){    // If the LL is Empty
             head = new Node(data);
@@ -27,12 +27,12 @@ public class SinglyLinkedList {
         }
     }
 
-
+     // This Method Removes the Node from the front of a Singly Linked List
     private static void removeFromFront(){
         head = head.next;
     }
 
-
+    // This Method Removes the Node from the Back of the Singly Linked List
     private static void removeFromBack(){
         if(head == null){
             return;
@@ -49,7 +49,7 @@ public class SinglyLinkedList {
         }
     }
 
-
+     // This Method Prints all The Elements of the Singly Linked List
     private static void printLL(){
         Node current = head;
         while(current != null){
@@ -59,6 +59,7 @@ public class SinglyLinkedList {
     }
 
 
+        // This is The Node Class to define the Structure of the Node it consists of two Constructors and 2 variables;
     private static class Node{
 
         private int data;   // Setting up the data and Node next
